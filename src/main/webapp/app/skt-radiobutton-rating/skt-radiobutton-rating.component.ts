997bedc229a5ba29807467037fac71fd3fea550e
johannes <73963446+coppeneur@users.noreply.github.com>
2022-02-11 12:08:09 +0100

    integrated radiobutton into skt-selfevaluation

--- src/main/webapp/app/skt-radiobutton-rating/skt-radiobutton-rating.component.ts
+++ src/main/webapp/app/skt-radiobutton-rating/skt-radiobutton-rating.component.ts
@@ -1,8 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'jhi-skt-radiobutton-rating',
   templateUrl: './skt-radiobutton-rating.component.html',
   styleUrls: ['./skt-radiobutton-rating.component.scss'],
 })
-export class SktRadiobuttonRatingComponent {}
+export class SktRadiobuttonRatingComponent {
+  @Input() selfevaluationID?: number;
+
+  rating?: number;
+  values: any = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  radioChangeHandler(event: any): void {
+    this.rating = event.target.value;
+  }
+}
