d135915c2ac3bb25fb31f96d9cdbfb12144e553a
Kilian <>
2022-02-18 12:34:51 +0100

    deleted findexpert-component

--- src/main/webapp/app/skt-findexpert/skt-findexpert.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'jhi-skt-findexpert',
-  templateUrl: './skt-findexpert.component.html',
-  styleUrls: ['./skt-findexpert.component.scss'],
-})
-export class SktFindexpertComponent {}
