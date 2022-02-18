3a9a72ce9d5f3c9bb969714667249dff43324a3b
Tommy Tran <tommy.tran@inform-software.com>
2022-02-18 22:16:54 +0100

    frontend: add chooseTeam button

--- src/main/webapp/app/account/settings/settings.component.ts
+++ src/main/webapp/app/account/settings/settings.component.ts
@@ -5,25 +5,40 @@ import { TranslateService } from '@ngx-translate/core';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
 import { LANGUAGES } from 'app/config/language.constants';
+import { ITeam, Team } from '../../entities/team/team.model';
+import { TeamComponent } from '../../entities/team/list/team.component';
+import { TeamService } from '../../entities/team/service/team.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-settings',
   templateUrl: './settings.component.html',
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent extends TeamComponent implements OnInit {
   account!: Account;
   success = false;
   languages = LANGUAGES;
+  team?: ITeam;
   settingsForm = this.fb.group({
     firstName: [undefined, [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
     lastName: [undefined, [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
     email: [undefined, [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email]],
+    teamKey: [undefined],
     langKey: [undefined],
   });
 
-  constructor(private accountService: AccountService, private fb: FormBuilder, private translateService: TranslateService) {}
+  constructor(
+    private accountService: AccountService,
+    private fb: FormBuilder,
+    private translateService: TranslateService,
+    protected teamService: TeamService,
+    protected modalService: NgbModal
+  ) {
+    super(teamService, modalService);
+  }
 
   ngOnInit(): void {
+    super.ngOnInit();
     this.accountService.identity().subscribe(account => {
       if (account) {
         this.settingsForm.patchValue({
@@ -45,6 +60,10 @@ export class SettingsComponent implements OnInit {
     this.account.lastName = this.settingsForm.get('lastName')!.value;
     this.account.email = this.settingsForm.get('email')!.value;
     this.account.langKey = this.settingsForm.get('langKey')!.value;
+    this.team = this.settingsForm.get('teamKey')!.value;
+
+    this.team?.teamMembers?.push(this.account);
+    this.teamService.update(this.team!);
 
     this.accountService.save(this.account).subscribe(() => {
       this.success = true;
